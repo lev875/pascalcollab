861ae9bac8921b2e0ea63dc93aca28ac682d34c7
lev875 <lev875gmail.com>
2017-01-18 21:03:33 +0300

    2

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.sendFile('index.html');
+app.get('/', function(req, res) {
+    res.sendFile('index.html'));
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(8080);
