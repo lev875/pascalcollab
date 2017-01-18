7d75cfb453ed1390e20a951360079d30730eede1
lev875 <lev875gmail.com>
2017-01-18 21:30:01 +0300

    2

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require("express");
 var app     = express();
-var path    = require("path");
 
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join('/index.html'));
 });
 
-app.listen(3000);
+app.listen(8080);
 
-console.log("Running at Port 3000");
+console.log("Running at Port 8080");
