4a8bbb997daa5283ec87fe83eb4608a927067eb5
lev875 <lev875gmail.com>
2017-01-18 21:56:56 +0300

    34

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app     = express();
+var path    = require("path");
 
 app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
-    response.sendFile('index.html');
+    response.sendFile(path.join(__dirname+'/index.html'));
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
