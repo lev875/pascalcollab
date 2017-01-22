26b0320e8b5c3d07f2c8bc3416f968da3efeb64b
lev875 <lev875gmail.com>
2017-01-22 17:28:58 +0300

    post

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ var path    = require("path");
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname+'/index.html'));
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
+});
+
+app.post("/", function (req, res) {
+    console.log(req.body.user.name)
 });
