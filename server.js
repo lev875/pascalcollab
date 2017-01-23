02728a25460f6de9088d6b7a8eb01910a3406af9
lev875 <lev875gmail.com>
2017-01-24 01:15:31 +0300

    input

--- server.js
+++ server.js
@@ -28,13 +28,15 @@ function OnCompile(request, response) {
     			console.log('stdout: ' + data);
 			});
 			compile.stderr.on('data', (data) => {
+				console.log(String(data));
+				buf += data;
+			});
+			compile.stderr.on('close', (data) => {
 				fs.unlink('temp.cpp', (err) => {
 					if (err) return console.error(err);
 					console.log('temp.cpp deleted (error)');
 				});
-				console.log(String(data));
-				buf += data;
-			});
+			})
 			compile.on('close', (data) => {
     			if (data === 0) {
         			var run = spawn('./a.out', []);
