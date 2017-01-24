98e0f9c0c8937944816c2f8304281f49a3e8f742
lev875 <lev875gmail.com>
2017-01-24 19:41:47 +0300

    hui

--- server.js
+++ server.js
@@ -24,18 +24,15 @@ function OnCompile(request, response) {
     fs.writeFile(name + '.cpp', request.body.code, 'utf8', (err) => {
   		if (err) return console.error(err);
    		else {
-    		var compile = spawn('g++',['-o', name + '.out', name + '.cpp', '-Werror']);
-			var res = {
-				output: '',
-				errors: ''
-			};
+    		var compile = spawn('g++',['-o', name + '.out', name + '.cpp']);
+			var buf = '';
 	
 			compile.stdout.on('data', (data) => {
     			console.log('stdout: ' + data);
 			});
 			compile.stderr.on('data', (data) => {
-				console.log(String(data));
-				res.errors += data;		
+				console.log('stderr: 'data);
+				buf += data;		
 			});
 			compile.on('close', (data) => {
 				fs.unlink(name + '.cpp', (err) => {
@@ -44,29 +41,31 @@ function OnCompile(request, response) {
 				});
     			if (data === 0) {
         			var run = spawn('./' + name + '.out', []);
+
         			setTimeout(function(){console.log(name + '.out killed'); run.kill()}, 5000);
 
 					if(request.body.input != '') {
 						run.stdin.write(request.body.input);
 						run.stdin.end();
-					}        	
+					}
         			run.stdout.on('data', (output) => {
             			console.log(String(output));
-            			res.output += output;
+            			buf += output;
         			});
         			run.stderr.on('data', (output) => {
-            			console.log(String(output));
-            			res.errors += output;
+            			console.log('stderr: ' + output);
+            			buf += output;
         			});
         			run.on('close', (output) => {
             			console.log('stdout: ' + output);
-            			response.send(JSON.stringify(res));
+            			response.send(buf);
 						fs.unlink(name + '.out', (err) => {
 							if (err) return console.error(err);
 							console.log(name + '.out deleted');
 						});
         			})
-    			} response.send(JSON.stringify(res));
+    			}
+    			response.send(buf);
 			})
 		}
 	});
