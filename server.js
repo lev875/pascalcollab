5bf33e17866d7ffd71b577c957a238772b8300b9
koyri <koy.ri.manrik@gmail.com>
2017-03-02 16:23:01 +0300

    pascal server code

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+console.log("Server started at port " + app.get('port'));
+
 function OnRequest(request, response){
     response.sendFile(path.join(__dirname+'/index.html'));
 }
@@ -21,32 +23,55 @@ function OnRequest(request, response){
 function OnCompile(request, response) {
 	var name = shortid.generate();
 
-    fs.writeFile(name + '.cpp', request.body.code, 'utf8', err => {
+    fs.writeFile(name + '.pas', request.body.code, 'utf8', err => {
   		if (err) return console.error(err);
-
    		else {
-    		var compile = spawn('g++',['-o', name + '.out', name + '.cpp']);
+   			try {
+    			var compile = spawn('fpc', [name + '.pas']);
+    		}
+    		catch (err) {
+    			console.error(err)
+    		}
 			var res = {
 				output: '',
 				err: '',
+				ErrorsParse: function (name) {
+					var x = this.err.split('\n')
+					var i
+	
+					for (i = 0; i < x.length; i++) {
+						if(x[i].search(name + '.pas\\(') === -1) {
+							x.splice(i,1)
+							i--;
+						}
+				  	}
+					for (i = 0; i < x.length; i++) {
+						x[i] = x[i].slice(name.length + 4)
+					}
+					this.err = x.join('\n')
+				}
 			};
 	
 			compile.stdout.on('data', data => {
     			console.log('stdout: ' + data);
+				res.err += data;
 			});
 			compile.stderr.on('data', data => {
 				res.err += data;		
 			});
 			compile.on('close', data => {
-				fs.unlink(name + '.cpp', err => {
+				fs.unlink(name + '.pas', err => {
 					if (err) return console.error(err);
-					
-					console.log(name + '.cpp deleted');
+					console.log(name + '.pas deleted');
+				});
+				fs.unlink(name + '.o', err => {
+					if (err) return console.error(err);
+					console.log(name + '.o deleted');
 				});
     			if (data === 0) {
-        			var run = spawn('./' + name + '.out', []);
+        			var run = spawn('./' + name, []);
 
-        			setTimeout(() => {console.log(name + '.out killed'); run.kill()}, 5000);
+        			setTimeout(() => {console.log(name + ' killed'); run.kill()}, 5000);
 
 					if(request.body.input != '') {
 						run.stdin.write(request.body.input);
@@ -59,26 +84,22 @@ function OnCompile(request, response) {
             			res.err += output;
         			});
         			run.on('close', output => {
-            			response.json(res);
-						fs.unlink(name + '.out', err => {
+						res.ErrorsParse(name)
+						response.json(res)
+						fs.unlink(name, err => {
 							if (err) return console.error(err);
-							console.log(name + '.out deleted');
+							console.log(name + ' deleted');
 						});
         			})
-    			} else response.json(res);
+    			} else {
+					res.ErrorsParse(name)
+					response.json(res)
+				}
 			})
 		}
 	});
 }
 
 app.get('/', OnRequest).listen(app.get('port'))
-
-app.get('/assets/styles.css', (request, response) => {
-	response.sendFile(path.join(__dirname+'/assets/styles.css'))
-})
-
-app.get('/assets/scripts.js', (request, response) => {
-	response.sendFile(path.join(__dirname+'/assets/scripts.js'))
-})
 
 app.post('/compile', OnCompile)
